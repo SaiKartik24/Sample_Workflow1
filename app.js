ea705c94cd6dde75f0dffdc92fe7dc42043e5e8d
SaiKartik24 <76741016+SaiKartik24@users.noreply.github.com>
2024-05-16 08:06:02 -0400

    Update app.js
    added new route inapp.js

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
     res.send('Sample Git Hub Actions');
 });
 
+app.get('/api', (req, res) => {
+    res.send('ping api data');
+});
+
 app.listen(port, () => { console.log('Our App Is Up And Running on!'+port); });
 module.exports = app
