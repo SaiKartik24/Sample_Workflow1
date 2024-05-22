55707452f9b17a3b7cf1ecd901f286a75a713fd6
Your Name <snistala@miraclesoft.com>
2024-05-22 21:21:47 +0530

    Added Date& time

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const port = process.env.port || 8080;
 const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Sample Git Hub Actions');
 });
 
-app.get('/api', (req, res) => {
-    res.send('ping api data');
-});
+app.get('/showDate', (req, res) => {
+    const d = new Date();
+    let textDate = d.toLocaleDateString();
+    let textTime = d.toLocaleTimeString()
+    res.send("Curent Date:"+ textDate+ '\n'+ "Present Time:"+ textTime);
+    //res.send(__dirname + '/index.html');
+  });
 
 app.listen(port, () => { console.log('Our App Is Up And Running on!'+port); });
 module.exports = app
